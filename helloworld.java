c18d8a9a2d724e9bb69d152c8fe01b150f011fbc
jilanisyed1 <jilanisyed1@gmail.com>
2017-05-09 11:27:54 -0200

    aall

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-    public class helloworld {
-=======
 public class helloworld {
->>>>>>> ju
     public static void main(String args[]){
         System.out.println("I'm dancing");
     }
